ccabe1ba3f46c5da6445d94f2f536e732fbc689b
Kurtis Moxley <image1224@outlook.com>
2026-05-19 23:11:32 +0800

    Init created.

---
+++ init.el
@@ -0,0 +1,8 @@
+(use-package emacs
+             :config
+             (tool-bar-mode -1)
+             (scroll-bar-mode -1)
+             (menu-bar-mode -1)
+             (setq line-number-mode t)
+             (setq column-number-mode t)
+             (setq inhibit-startup-screen t))
