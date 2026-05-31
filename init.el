da7bf428f8ba358ae5096d7f3208d99334f1e803
Kurtis Moxley <image1224@outlook.com>
2026-05-31 10:12:55 +0800

    Update display lines settings.

--- init.el
+++ init.el
@@ -3,7 +3,13 @@
              (tool-bar-mode -1)
              (scroll-bar-mode -1)
              (menu-bar-mode -1)
-             (add-hook 'prog-mode-hook 'display-line-numbers-mode)
              (setq line-number-mode t)
              (setq column-number-mode t)
-             (setq inhibit-startup-screen t))
+             (setq inhibit-startup-screen t)
+	     :hook
+	     (prog-mode .
+		(lambda ()
+			 (progn
+			  (display-line-numbers-mode 1)
+			  (setq display-line-numbers-type 'relative))))
+	     )
