39e9dab8a7fee171ed33d6f0b5b9cb2e55eab95e
Kurtis Moxley <image1224@outlook.com>
2026-05-31 08:17:36 +0800

    Add line numbers settings.

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
              (tool-bar-mode -1)
              (scroll-bar-mode -1)
              (menu-bar-mode -1)
+             (add-hook 'prog-mode-hook 'display-line-numbers-mode)
              (setq line-number-mode t)
              (setq column-number-mode t)
              (setq inhibit-startup-screen t))
